280071ca51e6bacf74f7dfbfe081bad6b774d40e
Anderson Laurentino <alaurentino.br@gmail.com>
2017-07-12 21:56:58 -0300

    Acionado um acendor e uma valvula no forno

--- Forno/Forno.ino
+++ Forno/Forno.ino
@@ -1,19 +1,34 @@
-#define tempPin 4
-#define co2Pin A0
+#include <Servo.h>
 
-#define velaPin 5
+#define co2Pin A0
+#define tempPin A1
+#define inicioBtn 2
+
+#define valvulaPin 5
+Servo valvula;
+
+#define acendedorEletrico 3
+
+bool estaQueimando;
+long inicioQueima;
 #define queimaLedPin 6
-#define duracaoQueima 1000
+#define duracaoQueima 30000
 
 void setup() {
-  pinMode(velaPin, OUTPUT);
+  estaQueimando = false;
+  inicioQueima = 0;
+
+  pinMode(inicioBtn, INPUT);
+  pinMode(acendedorEletrico, OUTPUT);
   pinMode(queimaLedPin, OUTPUT);
+  valvula.attach(valvulaPin);
+  
   Serial.begin(9600);
 }
 
-void loop() {
-  float temp = (5.0 * analogRead(tempPin) * 100.0) / 1024;
+void MostrarSensores() {
   float co2 = map(analogRead(co2Pin), 0, 1023, 0, 100);
+  float temp = (float(analogRead(tempPin))*5/(1023))/0.01;
 
   Serial.print("Temperatura: ");
   Serial.print(temp);
@@ -22,17 +37,69 @@ void loop() {
   Serial.print("Nivel de CO2: ");
   Serial.print(co2);
   Serial.println("%\n");
-
-  pulseTime(velaPin, 300);
-  digitalWrite(queimaLedPin, HIGH);
-  delay(duracaoQueima);
-  digitalWrite(queimaLedPin, LOW);
-
-  // Apagar()
 }
 
-void pulseTime(int port, int mili) {
-  digitalWrite(port, HIGH);
-  delay(mili);
-  digitalWrite(port, LOW);
+//
+// Inicia o processo de queima
+//
+void IniciarProcesso() {
+  Serial.println("\n\n------------------------\n");
+  Serial.println("> Iniciando a queima!");
+  
+  // Aciona a valula de gás
+  valvula.write(90);
+  Serial.println("> Abrindo a valvula!");
+  delay(2500);
+
+  // Acender o fogo
+  digitalWrite(acendedorEletrico, HIGH);
+  delay(1200);
+  digitalWrite(acendedorEletrico, LOW);
+  Serial.println("> Acendendo o fogo!");
+  
+  estaQueimando = true;
+  inicioQueima = millis();
+
+  digitalWrite(queimaLedPin, HIGH);
+  Serial.println("\n>> ATENÇÃO! Forno ligado!");
+  Serial.println("\n------------------------");
+}
+
+//
+// O processo de queima aguarda o fim do processo e deliga o sistema
+//
+void ProcessoQueima() {
+  // Verifica se o tempo de queima já foi atingido
+  if(millis() - inicioQueima >= duracaoQueima) {
+    Serial.println("\n> QUEIMA FINALIZADA!");
+    digitalWrite(queimaLedPin, LOW);
+    
+    estaQueimando = false;
+    inicioQueima = 0;
+    
+    valvula.write(0);
+    delay(2500);
+  }
+}
+
+void loop() {
+  // Mostra os valores dos sensores no Serial
+  MostrarSensores();
+
+  // Aguarda o botão para iniciar o processo
+  if(digitalRead(inicioBtn) == HIGH && !estaQueimando)
+    IniciarProcesso();
+
+  // Verifica se esta queimando
+  else if(estaQueimando) {
+    ProcessoQueima();
+    delay(3000);
+  }
+
+  // Os led piscando indica estado de latância
+  else {
+    digitalWrite(queimaLedPin, HIGH);
+    delay(700);
+    digitalWrite(queimaLedPin, LOW);
+  }
 }
